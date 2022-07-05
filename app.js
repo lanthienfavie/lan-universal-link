314122c52187ea58dc3c4114fd326642787321f3
Lan Thien <lan.thiendiep@gmail.com>
2022-07-05 13:26:57 +0700

    add

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+
+var credentials = { key: fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem') };
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+var options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+
+httpsServer.listen(443);
+
+app.get('/apple-app-site-association', (req, res) => {
+    // var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
+    // res.send(aasa);
+    res.download(__dirname + '/apple-app-site-association');
+});
