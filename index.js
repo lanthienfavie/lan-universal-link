25c0ced75162042982ef5ff1f6521efb2f0709ae
Lan Thien <lan.thiendiep@gmail.com>
2022-07-05 14:28:48 +0700

    no message

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require('express')
 const app = express()
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
-
-
-var credentials = { key: fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem') };
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-
-app.listen(process.env.PORT || 3000);
-
-httpsServer.listen(443);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -23,3 +11,5 @@ app.get('/apple-app-site-association', (req, res) => {
     // res.send(aasa);
     res.download(__dirname + '/apple-app-site-association');
 });
+
+app.listen(process.env.PORT || 3000);
