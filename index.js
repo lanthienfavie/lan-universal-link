1eb71924a95ff2e46baabb1a25a69ade5dab47be
Lan Thien <lan.thiendiep@gmail.com>
2022-07-14 14:31:50 +0700

    no message

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-app.get('/apple-app-site-association', (req, res) => {
+app.get('/.well-known/apple-app-site-association', (req, res) => {
     // var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
     // res.send(aasa);
-    res.download(__dirname + '/apple-app-site-association');
+    res.download(__dirname + '/.well-known/apple-app-site-association');
 });
 
 app.listen(process.env.PORT || 3000);
